eda652bd0879a93e5c33370ac1e90189147a6e73
zealotzealot <hsh0908y@gmail.com>
2019-11-29 01:12:20 +0900

    Implement radio button for selecting recognization type

--- android/app/src/main/java/com/example/hsh0908y/auto_wifi/ProcessingActivity.java
+++ android/app/src/main/java/com/example/hsh0908y/auto_wifi/ProcessingActivity.java
@@ -6,10 +6,15 @@ import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.io.File;
 
 public class ProcessingActivity extends AppCompatActivity {
+    private final String TAG = "ProcessingActivity";
+
+    private Bitmap bitmap;
+    private boolean hasId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +22,18 @@ public class ProcessingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_processing);
 
         Intent intent = getIntent();
+
         String imagePath = intent.getStringExtra("imageFile");
         File imageFile = new File(imagePath);
-        Bitmap bitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
-
+        bitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
         imageFile.delete();
 
-        // Temporary: show image
+        hasId = intent.getBooleanExtra("hasId", true);
+
+        // Temporary: show image and hasId
         ImageView imageView = (ImageView) findViewById(R.id.processingImageView);
         imageView.setImageBitmap(bitmap);
+        TextView textView = (TextView) findViewById(R.id.processingTextView);
+        textView.setText(String.valueOf(hasId));
     }
 }
