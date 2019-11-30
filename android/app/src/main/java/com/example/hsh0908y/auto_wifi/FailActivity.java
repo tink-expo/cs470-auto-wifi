d328b42c167e1ee1b7bb8dfa3bbcbcd7750d2e5a
zealotzealot <hsh0908y@gmail.com>
2019-11-30 23:17:51 +0900

    Implement changing PW in FailActivity

--- android/app/src/main/java/com/example/hsh0908y/auto_wifi/FailActivity.java
+++ android/app/src/main/java/com/example/hsh0908y/auto_wifi/FailActivity.java
@@ -1,10 +1,14 @@
 package com.example.hsh0908y.auto_wifi;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class FailActivity extends AppCompatActivity {
@@ -40,6 +44,25 @@ public class FailActivity extends AppCompatActivity {
         pwButton.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(FailActivity.this);
+                dialogBuilder.setTitle("Password");
+                final EditText input = new EditText(FailActivity.this);
+                input.setText(currentPw);
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+                dialogBuilder.setView(input);
+                dialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        setCurrentPw(input.getText().toString());
+                    }
+                });
+                dialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                dialogBuilder.show();
             }
         });
     }
