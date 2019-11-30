a2bae5651af1bd0b36acfde34f0a8c19b0d4bb05
tink-expo <whhan8485@gmail.com>
2019-11-30 23:31:55 +0900

    Before changing failure callback policy

--- android/app/src/main/java/com/example/hsh0908y/auto_wifi/WifiScanConnect.java
+++ android/app/src/main/java/com/example/hsh0908y/auto_wifi/WifiScanConnect.java
@@ -74,14 +74,15 @@ public class WifiScanConnect {
                     successIntent.putExtra("id", wifiManager.getConnectionInfo().getSSID());
                     activity.startActivity(successIntent);
                 } else {
+
                     if (intent.getAction().equals(WifiManager.SUPPLICANT_STATE_CHANGED_ACTION)) {
                         if (intent.hasExtra(WifiManager.EXTRA_SUPPLICANT_ERROR)) {
                             wifiManager.removeNetwork(netId);
                             Log.d(TAG, String.valueOf(intent.getIntExtra(WifiManager.EXTRA_SUPPLICANT_ERROR, -1)));
-                            Intent failIntent = new Intent(activity, FailActivity.class);
-                            failIntent.putExtra("id", ssidPw.ssid);
-                            failIntent.putExtra("pw", ssidPw.pw);
-                            activity.startActivity(failIntent);
+//                            Intent failIntent = new Intent(activity, FailActivity.class);
+//                            failIntent.putExtra("id", ssidPw.ssid);
+//                            failIntent.putExtra("pw", ssidPw.pw);
+//                            activity.startActivity(failIntent);
                         }
                     }
                 }
