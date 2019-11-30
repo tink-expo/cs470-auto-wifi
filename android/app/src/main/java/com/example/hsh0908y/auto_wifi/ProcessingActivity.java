c5838ad0752c2e522ecba62f1988514ffdafda9d
zealotzealot <hsh0908y@gmail.com>
2019-11-30 22:55:45 +0900

    Refactor WifiScanConnect.scanAndRegisterReceier()  to get receiver as parameter

--- android/app/src/main/java/com/example/hsh0908y/auto_wifi/ProcessingActivity.java
+++ android/app/src/main/java/com/example/hsh0908y/auto_wifi/ProcessingActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -18,6 +19,7 @@ import com.example.hsh0908y.auto_wifi.common.TextBlock;
 import com.example.hsh0908y.auto_wifi.common.WifiData;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProcessingActivity extends AppCompatActivity {
@@ -60,7 +62,26 @@ public class ProcessingActivity extends AppCompatActivity {
     @Override
     protected void onPostResume() {
         super.onPostResume();
-        wifiScanReceiver = WifiScanConnect.scanAndRegisterReceiver(this, wifiManager, new IntentFilter());
+        wifiScanReceiver = WifiScanConnect.scanAndRegisterReceiver(this, wifiManager, new IntentFilter(),
+                new BroadcastReceiver() {
+                    @Override
+                    public void onReceive(Context context, Intent intent) {
+                        ProcessingActivity activity = ProcessingActivity.this;
+                        if (activity == null || activity.isFinishing() || activity.getHasTriedWifiConnect()) {
+                            return;
+                        }
+
+                        List<ScanResult> scanResultList = wifiManager.getScanResults();
+                        List<WifiData> wifiDataList = new ArrayList<>();
+                        for (ScanResult scanResult : scanResultList) {
+                            int signalLevel = wifiManager.calculateSignalLevel(scanResult.level, 5);
+                            wifiDataList.add(new WifiData(scanResult.SSID, signalLevel));
+                            Log.d(TAG, scanResult.SSID + ", " + String.valueOf(signalLevel));
+                        }
+                        activity.setReceivedWifiDataList(wifiDataList);
+                        activity.tryWifiConnect();
+                    }
+                });
         TextDetect.detectAndRegisterCallback(this, unscaledBitmap);
     }
 
